7f46b186eb46ae8e3e85649b43956758a9c7b90f
tkamal98 <108579190+Kamal-13-Sing@users.noreply.github.com>
2024-07-18 15:50:42 +0545

    Add files via upload

---
+++ TestJenkins.java
@@ -0,0 +1,12 @@
+package testjava;
+
+public class TestJenkins {
+	
+	public static void main(String []args) {
+		
+		System.out.println("******************");
+		System.out.println("Hello From Github");
+		System.out.println("$$$$$$$$$$$$$$$$$");
+	}
+
+}
